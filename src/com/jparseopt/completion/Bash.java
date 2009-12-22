f88da4f5ff6a1f8da67629bd616638e6f77db451
teneighty <tmhorton@gmail.com>
2009-12-22 10:39:33 -0800

    Added one level of opt support for bash.

--- src/com/jparseopt/completion/Bash.java
+++ src/com/jparseopt/completion/Bash.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 
+import java.util.Collection;
 import java.util.Map;
 
 /**
@@ -22,19 +23,45 @@ public class Bash
         System.out.printf("\tCOMPREPLY=() \n");
         System.out.printf("\tcur=\"${COMP_WORDS[COMP_CWORD]}\" \n");
         System.out.printf("\tprev=\"${COMP_WORDS[COMP_CWORD-1]}\" \n");
-        System.out.printf("\topts=\"");
+        System.out.printf("\topts=\"%s\" \n\n", createOpts(options.values()));
+
+        System.out.printf("\tcase \"${prev}\" in \n");
         for (Option o : options.values()) {
-            if (o.getHidden())
+            if (o.getOptMap().size() == 0)
                 continue;
 
-            for (String s : o.getArgs()) {
-                System.out.printf("%s ", s);
-            }
+            String pattern = createPattern(o.getArgs());
+            String children = createOpts(o.getOptMap().values());
+            System.out.printf("\t\t%s) \n", pattern);
+            System.out.printf("\t\t\tlocal v=\"%s\" \n ", children);
+            System.out.printf("\t\t\tCOMPREPLY=( $(compgen -W \"${v}\" -- ${cur}) ) \n ");
+            System.out.printf("\t\t\treturn 0 \n ");
+            System.out.printf("\t\t\t;; \n");
         }
-        System.out.printf("\" \n");
+        System.out.print("\tesac \n ");
+
         System.out.printf("\tCOMPREPLY=( $(compgen -W \"${opts}\" -- ${cur}) ) \n");
         System.out.printf("\treturn 0 \n");
         System.out.printf("} \n");
         System.out.printf("complete -F %s %s\n", func, cmd);
+    }
+
+    private String createPattern(String[] args) {
+        String delim = " | ";
+        StringBuffer buf = new StringBuffer("");
+        for (String a : args) {
+            buf.append("\"").append(a).append("\"").append(delim);
+        }
+        return buf.toString().replaceAll(" \\| $", "");
+    }
+
+    private String createOpts(Collection <Option> os) {
+        StringBuffer buf = new StringBuffer("");
+        for (Option o : os) {
+            for (String a : o.getArgs()) {
+                buf.append(a).append(" ");
+            }
+        }
+        return buf.toString();
     }
 }
